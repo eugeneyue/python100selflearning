0a7f635a4a6cb7eb0dfa7f343f3223d575c29d89
eugeneyue <yujiang.yue@ifchange.com>
2019-05-09 20:54:57 +0800

    0509

---
+++ day02.py
@@ -0,0 +1,92 @@
+# a = 321
+# b=123
+#
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+#-------------------------------------
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+#
+# print('%d + %d = %d' % (a, b, a + b))
+# print('%d - %d = %d' % (a, b, a - b))
+# print('%d * %d = %d' % (a, b, a * b))
+
+#-------------------------------------
+
+# a = 100
+# b = 12.345
+# c = 1 + 5j
+# d = 'Hello World'
+# e = True
+#
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+# z = 100000000000000000000000000000
+# print(type(z))
+
+#-------------------------------------
+
+# a = 5
+# b = 10
+# c = 3
+# d = 4
+# e = 5
+# a += b
+# a -= c
+# a *= d
+# a /= e
+#
+# print("a = ", a)
+#
+# flag1 = 3 > 2               #true
+# flag2 = 2 < 1               #false
+# flag3 = flag1 and flag2     #false
+# flag4 = flag1 or flag2      #true
+# flag5 = not flag1           #false
+#
+# print("flag1 = ", flag1)
+# print("flag2 = ", flag2)
+# print("flag3 = ", flag3)
+# print("flag4 = ", flag4)
+# print("flag5 = ", flag5)
+#
+# print(flag1 is True)
+# print(flag2 is not False)
+
+#-------------------------------------
+
+# f = float(input("请输入华氏温度： "))
+# c = (f - 32) / 1.8
+# print('%.1f华氏度 = %.1f摄氏度' % (f , c))
+
+#-------------------------------------
+
+# pi = 3.1415926
+# r = float(input("请输入半径： "))
+# c = 2 * pi * r
+# s = pi * r * r
+#
+# print('圆周长为%.3f' % c)
+# print('圆面积为%.3f' % s)
+
+#-------------------------------------
+
+# year = int(input("Please input year: "))
+#
+# is_leap = (year % 4 == 0 and year % 100 != 0 or year % 400 == 0)
+#
+# print(is_leap)
+
+#-------------------------------------
+
