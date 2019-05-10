806e1e556885abcdf1c6a9119d6c33c6d373f724
eugeneyue <yujiang.yue@ifchange.com>
2019-05-10 17:35:01 +0800

    0510

---
+++ day04.py
@@ -0,0 +1,49 @@
+# sum = 0
+#
+# for x in range(101):
+#     sum += x
+# print(sum)
+
+#-------------------------------------
+
+# sum1 = 0
+# for y in range(0,101,2):
+#     sum1 += y
+# print(sum1)
+
+#-------------------------------------
+
+# sum = 0
+# for x in range(2, 101, 2):
+#     sum += x
+# print(sum)
+
+#-------------------------------------
+
+# sum = 0
+#
+# for x in range(1,101):
+#     if x % 2 == 0:
+#         sum += x
+# print(sum)
+
+#-------------------------------------
+
+# from random import randint
+#
+# num = randint(1,101)
+# counter = 0
+#
+# while(True):
+#     called = int(input("Please enter a number: "))
+#     counter += 1
+#     if(called > num):
+#         print("Smaller than your num!")
+#     elif(called == num):
+#         print("Cool!")
+#         break
+#     else:
+#         print("Bigger than your num!")
+
+#-------------------------------------
+
