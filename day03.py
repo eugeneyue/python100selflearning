020aaf22517edc4f991f7b72aea4f82e85bc23d0
eugeneyue <yujiang.yue@ifchange.com>
2019-05-10 11:52:01 +0800

    0510

--- day03.py
+++ day03.py
@@ -1,1 +1,38 @@
-123
+# import getpass
+#
+# username = input("请输入用户名： ")
+# #password = input("请输入口令： ")
+# password = getpass.getpass("请输入口令： ")
+#
+# if  username == 'admin' and password == '123456':
+#     print("Success!")
+# else:
+#     print("Failed！")
+
+#-------------------------------------
+
+# x = float(input('x = '))
+# if  x > 1:
+#     y = 3 * x - 5
+#
+# elif x >= -1:
+#     y = x + 2
+#
+# else:
+#     y = 5 * x + 3
+#
+# print('f(%.2f) = %.2f' % (x,y))
+
+#-------------------------------------
+
+# value = float(input('请输入长度： '))
+# unit = input('请输入单位： ')
+#
+# if  unit == 'in' or unit == '英寸':
+#     print('%f 英寸 = %f 厘米' % (value , value * 2.54))
+# elif unit == 'cm' or unit == '厘米':
+#     print('%f厘米 = %f英寸' % (value, value / 2.54))
+# else:
+#     print('请输入有效的单位')
+
+#-------------------------------------
